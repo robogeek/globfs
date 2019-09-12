3dbe0a6f7a2e4ca088b73782d1dec3e291926bf0
David Herron <david@davidherron.com>
2019-09-12 15:49:28 -0700

    Switch to async functions, update dependencies

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ const glob  = require('glob');
 const fs    = require('fs-extra');
 const util  = require('util');
 const path  = require('path');
-const co    = require('co');
 
 function doTracing() {
     return (typeof process.env.GLOBFS_TRACE !== 'undefined'
@@ -35,7 +34,13 @@ function doTracing() {
      ));
 }
 
-module.exports.operateAsync = co.wrap(function* (basedirs, patterns, operation) {
+/**
+ * Glob based generic file operations from single or multiple source directories, and against
+ * single or multiple glob patterns.
+ *
+ * The signature for "operation" is function(basedir, path, function(err, result)).
+ */
+module.exports.operateAsync = async function(basedirs, patterns, operation) {
 
     var results = [];
 
@@ -60,7 +65,7 @@ module.exports.operateAsync = co.wrap(function* (basedirs, patterns, operation) 
 
     for (let basedir of basedirs) {
         for (let pattern of patterns) {
-            let files = yield new Promise((resolve, reject) => {
+            let files = await new Promise((resolve, reject) => {
                 glob(pattern, { cwd: basedir },
                     function(errGlob, files) {
                         if (errGlob) reject(errGlob);
@@ -74,9 +79,9 @@ module.exports.operateAsync = co.wrap(function* (basedirs, patterns, operation) 
                 if (trace) {
                     op_start = new Date();
                 }
-                let fresult = yield new Promise((resolve, reject) => {
+                let fresult = await new Promise((resolve, reject) => {
                     try {
-                        operation(basedir, fpath, (errOp, result) => {
+                        operation(basedir, fpath, async (errOp, result) => {
                             // console.log(`operateAsync finished operation result= ${basedir} ${fpath} ${util.inspect(errOp)} ${util.inspect(result)}`);
                             let op_end = trace ? new Date() : undefined;
                             let op_elapsed = trace ? (op_end - op_start) : undefined;
@@ -95,6 +100,7 @@ module.exports.operateAsync = co.wrap(function* (basedirs, patterns, operation) 
                                     basedir: basedir,
                                     path: fpath,
                                     fullpath: path.join(basedir, fpath),
+                                    stats: await fs.stat(path.join(basedir, fpath)),
                                     result: result,
                                     start: op_start,
                                     end: op_end,
@@ -114,7 +120,7 @@ module.exports.operateAsync = co.wrap(function* (basedirs, patterns, operation) 
     }
     // console.log(`operateAsync RESULTS ${util.inspect(results)}`);
     return results;
-});
+};
 
 /**
  * Glob based generic file operations from single or multiple source directories, and against
@@ -176,7 +182,7 @@ module.exports.findSync = function(basedirs, patterns) {
     return ret;
 };
 
-module.exports.copyAsync = co.wrap(function* (basedirs, patterns, destdir, options) {
+module.exports.copyAsync = async function(basedirs, patterns, destdir, options) {
 
     // util.log('copy '+ util.inspect(basedirs) +' '+ util.inspect(patterns) +' '+ destdir);
 
@@ -196,7 +202,7 @@ module.exports.copyAsync = co.wrap(function* (basedirs, patterns, destdir, optio
         throw new Error('incorrect destdir given '+ util.inspect(destdir));
     }
 
-    var files2copy = yield module.exports.operateAsync(basedirs, patterns);
+    var files2copy = await module.exports.operateAsync(basedirs, patterns);
     var results = "";
     for (var tocopy of files2copy) {
         // console.log(`copy ${util.inspect(tocopy)}`);
@@ -204,10 +210,10 @@ module.exports.copyAsync = co.wrap(function* (basedirs, patterns, destdir, optio
         var fnCopyTo   = path.join(destdir, tocopy.path);
         var dirCopyTo  = path.dirname(fnCopyTo);
 
-        var stats = yield fs.stat(fnCopyFrom);
+        var stats = await fs.stat(fnCopyFrom);
         if (! stats.isFile()) continue;
-        yield fs.mkdirs(dirCopyTo);
-        results += yield new Promise((resolve, reject) => {
+        await fs.mkdirs(dirCopyTo);
+        results += await new Promise((resolve, reject) => {
             var rd = fs.createReadStream(fnCopyFrom);
             /* eslint-disable no-console */
             rd.on("error", function(err) {
@@ -231,7 +237,7 @@ module.exports.copyAsync = co.wrap(function* (basedirs, patterns, destdir, optio
 
     return results;
 
-});
+};
 
 /**
  * Glob based file copying from single or multiple source directories, and against
@@ -247,7 +253,7 @@ module.exports.copy = function(basedirs, patterns, destdir, options, done) {
         .catch(err => { done(err); });
 };
 
-module.exports.rmAsync = co.wrap(function* (basedirs, patterns, options) {
+module.exports.rmAsync = async function(basedirs, patterns, options) {
 
     if (typeof basedirs === 'string') {
         var b = basedirs;
@@ -261,20 +267,20 @@ module.exports.rmAsync = co.wrap(function* (basedirs, patterns, options) {
 
     if (typeof options === 'undefined') options = {};
 
-    var files2rm = yield module.exports.operateAsync(basedirs, patterns);
+    var files2rm = await module.exports.operateAsync(basedirs, patterns);
     var results = "";
     for (var torm of files2rm) {
         var fn2Remove = path.join(torm.basedir, torm.path);
-        var stats = yield fs.stat(fn2Remove);
+        var stats = await fs.stat(fn2Remove);
         if (! stats.isFile()) continue;
-        yield fs.unlink(fn2Remove);
+        await fs.unlink(fn2Remove);
         if (options.verbose) {
             results += `RM ${fn2Remove}
 `;
         }
     }
     return results;
-});
+};
 
 /**
  * Glob based file deletion from single or multiple source directories, and against
@@ -290,7 +296,7 @@ module.exports.rm = function(basedirs, patterns, options, done) {
         .catch(err => { done(err); });
 };
 
-module.exports.chmodAsync = co.wrap(function* (basedirs, patterns, newmode, options) {
+module.exports.chmodAsync = async function(basedirs, patterns, newmode, options) {
     if (typeof basedirs === 'string') {
         var b = basedirs;
         basedirs = [ b ];
@@ -308,18 +314,18 @@ module.exports.chmodAsync = co.wrap(function* (basedirs, patterns, newmode, opti
 
     if (typeof options === 'undefined') options = {};
 
-    var files2chmod = yield module.exports.operateAsync(basedirs, patterns);
+    var files2chmod = await module.exports.operateAsync(basedirs, patterns);
     var results = '';
     for (var tochmod of files2chmod) {
         var fn2chmod = path.join(tochmod.basedir, tochmod.path);
-        yield fs.chmod(fn2chmod, newmode);
+        await fs.chmod(fn2chmod, newmode);
         if (options.verbose) {
             results += `CHMOD ${fn2chmod} ${newmode.toString(8)}
 `;
         }
     }
     return results;
-});
+};
 
 module.exports.chmod = function(basedirs, patterns, newmode, options, done) {
     module.exports.chmodAsync(basedirs, patterns, newmode, options)
@@ -327,7 +333,7 @@ module.exports.chmod = function(basedirs, patterns, newmode, options, done) {
         .catch(err => { done(err); });
 };
 
-module.exports.chownAsync = co.wrap(function* (basedirs, patterns, uid, gid, options) {
+module.exports.chownAsync = async function(basedirs, patterns, uid, gid, options) {
     if (typeof basedirs === 'string') {
         var b = basedirs;
         basedirs = [ b ];
@@ -346,18 +352,18 @@ module.exports.chownAsync = co.wrap(function* (basedirs, patterns, uid, gid, opt
         throw new Error('incorrect uid '+ util.inspect(uid) +' or gid '+ util.inspect(gid) +' given');
     }
 
-    var files2chown = yield module.exports.operateAsync(basedirs, patterns);
+    var files2chown = await module.exports.operateAsync(basedirs, patterns);
     var results = '';
     for (var tochown of files2chown) {
         var fn2chown = path.join(tochown.basedir, tochown.path);
-        yield fs.chown(fn2chown, uid, gid);
+        await fs.chown(fn2chown, uid, gid);
         if (options.verbose) {
             results += `CHOWN ${fn2chown} ${uid} ${gid}
 `;
         }
     }
     return results;
-});
+};
 
 module.exports.chown = function(basedirs, patterns, uid, gid, options, done) {
     module.exports.chownAsync(basedirs, patterns, uid, gid, options)
@@ -366,7 +372,7 @@ module.exports.chown = function(basedirs, patterns, uid, gid, options, done) {
 };
 
 
-module.exports.duAsync = co.wrap(function* (basedirs, patterns, options) {
+module.exports.duAsync = async function(basedirs, patterns, options) {
     if (typeof basedirs === 'string') {
         var b = basedirs;
         basedirs = [ b ];
@@ -380,19 +386,19 @@ module.exports.duAsync = co.wrap(function* (basedirs, patterns, options) {
     if (typeof options === 'undefined') options = {};
 
     var totalsz = 0;
-    var files2du = yield module.exports.operateAsync(basedirs, patterns);
+    var files2du = await module.exports.operateAsync(basedirs, patterns);
     var sizes = '';
     // console.log(util.inspect(files2du));
     for (var todu of files2du) {
         var fn2du = path.join(todu.basedir, todu.path);
-        var stats = yield fs.stat(fn2du);
+        var stats = await fs.stat(fn2du);
         sizes += `${fn2du} ${stats.size}
 `;
         totalsz += stats.size;
     }
     return options.verbose ? (`${sizes}
 TOTAL ${totalsz}`) : totalsz;
-});
+};
 
 module.exports.du = function(basedirs, patterns, options, done) {
     module.exports.duAsync(basedirs, patterns, options)
